612edfe1e95018820d46735081372a4e9b810bda
Kavindu Sanjitha <kavindusanga1331@gmail.com>
2025-11-18 10:21:57 +0530

    conflict fixed

--- frontend/blockchain-based-bidding-system/src/App.jsx
+++ frontend/blockchain-based-bidding-system/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/login";
-<<<<<<< Updated upstream
+
 import Signup from "./components/signup/signup";
-=======
+
 import Signup from "./components/Signup/signup";
 import Landing from "./components/landing_page/landing";
 import Upload from './components/Upload/Upload';
 
->>>>>>> Stashed changes
+
 
 function App() {
   return (
@@ -16,11 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< Updated upstream
-        
-=======
+
+
         <Route path="/upload" element={<Upload />} />
->>>>>>> Stashed changes
+
       </Routes>
     </Router>
   );
